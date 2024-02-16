a57ad97841639cc9a6700647286a5b22f73a47a3
lil-sussy <naykeysnet@gmail.com>
2024-02-16 19:33:09 +0100

    css improved

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let translationText = "Hello, World!";
+  export let translationText = "Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!";
   
   const socket = new WebSocket("ws://localhost:8001/translation-text");
   socket.onmessage = function (event) {
@@ -27,12 +27,16 @@
 		padding: 1rem;
 		max-width: 240px;
 		margin: 0 auto;
+    box-sizing: border-box;
+    
 	}
   .translation-container {
     display: flex;
     justify-content: center;
     align-items: end;
     height: 100%;
+    box-sizing: border-box;
+    padding-bottom: 4rem;
   }
   
 	.translation {
@@ -40,6 +44,8 @@
     color: #ffff;
 		font-size: 2rem;
 		font-weight: 500;
+    overflow: wrap;
+    width: 80%;
 	}
 
 	@media (min-width: 640px) {
